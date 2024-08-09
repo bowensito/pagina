7caae9c348ea79c7c0fcb2890cb7dacc31a60797
bowensito <bowensitosmurf@gmail.com>
2024-08-09 12:18:47 -0400

    Add files via upload

---
+++ codigo/Untitled-1.java
@@ -0,0 +1,19 @@
+
+int poblacion = 19329393320210
+Long distancia = 5620L 
+Float peso = 58,8 
+String saludo = "hola mundo"
+
+class Persona {
+    String nombre;
+    int edad;
+
+    void saludar() {
+        System.output.println ("Hola, mi nombre es " + nombre)
+    }
+}
+Persona p = nuevo nombre ();
+Letra letra = letras();
+Persoba y = lkjafdskf();
+
+y.nombre = carlos
